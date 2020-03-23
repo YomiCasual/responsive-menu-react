4646fb5bf34fabf86b1817526f0cbde4e09be995
YomiCasual <yomionisade@gmail.com>
2020-03-23 12:42:40 +0000

    2 files modified

--- components/Body.js
+++ components/Body.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import logo from './images/logo.png'
 
 export default () => (
   <div className="container">
-  <img src={logo} />
+  <div className="sub-container">
+  <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png" />
+  <h3 className="header">Tester</h3>
+  </div>
+  <div class="sub-container2">
+  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+  </div>
   </div>
 )
