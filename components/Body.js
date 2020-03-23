077e87f496239cab17198398e8a82360785a807e
YomiCasual <yomionisade@gmail.com>
2020-03-23 13:39:50 +0000

    2 files modified

--- components/Body.js
+++ components/Body.js
@@ -4,10 +4,17 @@ export default () => (
   <div className="container">
   <div className="sub-container">
   <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png" />
-  <h3 className="header">Tester</h3>
+  <h3 className="header">Sleek Touch</h3>
   </div>
   <div class="sub-container2">
-  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+  <div>
+  <p>const name = Yomi, <span>Onisade</span></p>
+  <p><span>Front-End Developer =></span>Javascript, React, Gatsby, Python</p>
+  <p><span>Graphic Designer =></span>Coreldraw, Photoshop</p>
+  <p><span>UI Designer=></span>Figma</p>
+  <p><span>Graphic Designer =></span>Coreldraw, Photoshop</p>
+  <p><span>Chemist =></span>Synthetic Chemist</p>
+  </div>
   </div>
   </div>
 )
