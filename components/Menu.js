d1d09c7ab31a4a83aef880755b7fcdefffa4bdf5
YomiCasual <yomionisade@gmail.com>
2020-03-23 13:35:41 +0000

    1 file modified

--- components/Menu.js
+++ components/Menu.js
@@ -3,17 +3,17 @@ import React from 'react'
 export default ({modalOpen, toggleModal}) => {
 
     const modalClass = modalOpen ? "menu-modal open" : "menu-modal"
-
     return (
       <div className={modalClass}>
       <div className="menu-items">
-      <h3>Home</h3>
-      <h3>Items</h3>
-      <h3>About</h3>
-      <h3>Learn More</h3>
-      <h3>Contact</h3>
+      <h4>Check my porfolio as</h4>
+      <h3>Front-End Developer</h3>
+      <h3>Graphic Designer</h3>
+      <h3>UI Designer</h3>
+      <h3>Brand Designer</h3>
+      <h3>Chemist</h3>
       </div>
-         <button className="close-modal" onClick={toggleModal}>X</button>
+       <button className="close-modal" onClick={toggleModal}>X</button>
       </div>
     )
 
